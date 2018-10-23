e296a9405481e049f7e246cccca2b9ee28af7da6
nzheng <nathan.zheng@kardium.com>
2018-10-23 11:41:40 -0700

    updated new invoice serialization test

--- test/Misc/NewInvoiceSerializationTest.java
+++ test/Misc/NewInvoiceSerializationTest.java
@@ -6,15 +6,24 @@ import org.junit.Test;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class NewInvoiceSerializationTest {
 
     @Test
     public void test() throws Exception {
-        File file = new File("Saves/1809050.dat");
-        Invoice invoice = Invoice.read(file);
-        try (ObjectOutputStream fos = new ObjectOutputStream(new FileOutputStream(new File("Saves/1809050Upgraded.dat")))) {
-            invoice.write(fos);
-        }
+        Files.walk(Paths.get("Saves")).map(Path::toFile).filter(File::isFile).forEach(file -> {
+            try {
+                Invoice invoice = Invoice.read(file);
+                try (ObjectOutputStream fos = new ObjectOutputStream(new FileOutputStream(
+                        new File("SavesUpgrade/" + invoice.getInvoiceNum() + ".dat")))) {
+                    invoice.write(fos);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } );
     }
 }
